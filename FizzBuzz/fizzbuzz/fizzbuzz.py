8af5edf4f9ffc9428a2d1b7499ee4ce94fa0e327
vikaBu <40745677+vikaBu@users.noreply.github.com>
2021-07-15 18:24:01 +0100

    Add files via upload

--- FizzBuzz/fizzbuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz/fizzbuzz.py
@@ -1,18 +1,22 @@
+import time
+
+
 def fizzbuzz(start, end):
     def int_to_fizzbuzz(i):
         entry = ''
         if i%3 == 0:
-            entry += "fizz"
+            yield "fizz"
         if i%5 == 0:
-            entry += "buzz"
+            yield "buzz"
         if i%3 != 0 and i%5 != 0:
-            entry = i
-        return entry
+            yield "fizzbuzz"
 
-    return [int_to_fizzbuzz(i) for i in range(start, end+1)]
+    yield (int_to_fizzbuzz(i) for i in range(start, end+1))
+
+
 
 if __name__ == "__main__":
     start = int(input("Start Value:"))
     end = int(input("End Value:"))
-    for i in fizzbuzz(start, end):
-        print(i)
+    for i in fizzbuzz (start, end):
+        print (list(i))
