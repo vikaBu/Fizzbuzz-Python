0a80215c25cce7cd873e4f2746a51a66ddf6135e
vikaBu <40745677+vikaBu@users.noreply.github.com>
2021-07-15 08:03:57 +0100

    Add files via upload

---
+++ FizzBuzz/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzbuzz(number):
+    answer = []
+
+    for current_number in range(1, number + 1):
+        if current_number % 3 == 0:
+            answer.append("Fizz")
+        elif current_number % 5 == 0:
+            answer.append("Buzz")
+        elif current_number % 3 == 0 and current_number % 5 == 0:
+            answer.append("FizzBuzz")
+        else:
+            answer.append(f"{current_number}")
+
+    return answer
+
+
+print(fizzbuzz(15))
